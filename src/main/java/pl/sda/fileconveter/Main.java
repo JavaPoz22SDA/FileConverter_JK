f79027a463e6d5282e482f856649a9a84b36007d
jacekkokot <jacekkokot.ue@gmail.com>
2020-05-16 09:36:41 +0200

    add gitignore

--- src/main/java/pl/sda/fileconveter/Main.java
+++ src/main/java/pl/sda/fileconveter/Main.java
@@ -1,4 +1,7 @@
 package pl.sda.fileconveter;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/pl/sda/fileconveter/Reader.java
+++ src/main/java/pl/sda/fileconveter/Reader.java
@@ -1,4 +1,5 @@
 package pl.sda.fileconveter;
 
 public class Reader {
+
 }
